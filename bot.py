d4155b5b43c6225e9ed2b85c5b152415ae2f73d0
TheBicPen <mashkal2000@gmail.com>
2021-01-17 00:07:20 -0800

    Improve data storage. Add more management commands.

--- bot.py
+++ bot.py
@@ -11,18 +11,21 @@ SAME_DIFF=20
 HASH_SIZE=16
 
 default_strings = {
-    "scan": "reposti !scan",
+    "scan": "reposti scan",
     # "scan_all": "reposti !scanall",
-    "disable": "reposti !disable",
-    "enable": "reposti !enable",
+    "disable": "reposti disable",
+    "enable": "reposti enable",
+    "exclude": "reposti exclude",
+    "include": "reposti include",
     "hello": "hi reposti",
-    "hash": "reposti !hash",
-    "hashdiff": "hashdiff",
+    "hash": "reposti hash",
+    "hashdiff": "reposti diff",
+    "diff": "reposti diff",
     "repost_found": "General reposti"
-}
-command_strings = default_strings.copy()
 
-data={}
+}
+
+
 
 def hash_diff(h1, h2):
     """
@@ -63,60 +66,68 @@ def image_hash_from_message(message):
             print(e, url)
     return out
 
-async def scan_channel(channel, history_args):
+async def scan_channel(channel, data, history_args):
     print(f"Scanning '#{channel.name}', {'all' if history_args.get('limit') is None else history_args['limit']} posts")
     await channel.send("Scanning posts...")
     scanned_posts = 0
     images_found = 0
     image_errors = 0
-    hashes=[]
+    hashes={}
     async with channel.typing():
         async for m in channel.history(**history_args):
             if scanned_posts % 100 == 0:
                 print(f"Scanned {scanned_posts} posts in '#{channel.name}'") #, m.jump_url)
             embeds = image_hash_from_message(m)
-            hashes.extend(embeds["hashes"])
+            for h in embeds["hashes"]:
+                if h in hashes:
+                    hashes[h].append((channel.id, m.id))
+                else:
+                    hashes[h] = [(channel.id, m.id)]
+            
             images_found += len(embeds["hashes"]) + embeds["errors"]
             image_errors += embeds["errors"]
             scanned_posts += 1
-    hashes=list(set(hashes)) # switch to using sets and implement a JSON codec
-    guild_name, server_file = unique_guild_data(channel.guild)
-    if not data[guild_name].get("hashes"):
-        data[guild_name]["hashes"] = []
-    data[guild_name]["hashes"].extend(hashes)
-    data[guild_name]["hashes"] = list(set(data[guild_name]["hashes"]))
+    add_hash_data(data, channel.guild, hashes)
+    print(f"Done. Scanned {scanned_posts} posts, found {len(hashes)} unique images, {image_errors} errors.")
+    await channel.send(f"Done. Scanned {scanned_posts} posts, found {len(hashes)} unique images, {image_errors} errors.")
+
+def save_guild_data(data, guild):
+    guild_name, server_file = unique_guild_data(guild)
     with open(server_file, "w") as f:
         json.dump(data[guild_name], f)
-    print(f"Done. Scanned {scanned_posts} posts, found {images_found} images, {len(hashes)} unique images, {image_errors} errors.")
-    await channel.send(f"Done. Scanned {scanned_posts} posts, found {images_found} images, {len(hashes)} unique images, {image_errors} errors.")
 
-client = discord.Client()
+def get_guild_data(data, guild, k, default=None):
+    guild_name, _ = unique_guild_data(guild)
+    return data[guild_name].get(k, default)
 
-def unique_guild_data(guild):
-    unique_name = "".join([x for x in guild.name if x.isalnum()]) + "_" + str(guild.id)
-    file_name = path.join("data", unique_name + ".json")
-    return (unique_name, file_name)
+def set_guild_data(data, guild, k, v):
+    guild_name, _ = unique_guild_data(guild)
+    data[guild_name][k] = v
+    save_guild_data(data, guild)
 
-def check_message(message, max_diff=0):
-    embeds = image_hash_from_message(message)
-    guild_name, _ = unique_guild_data(message.guild)
-    for h in embeds["hashes"]:
-        if max_diff == 0:
-            if h in data[guild_name]["hashes"]:
-                print("Found matching hash", h)
-                return True
+def add_hash_data(data, guild, hashes: dict):
+    """
+    Adds hashes to data.
+    hashes should be a dict that maps the hash strings to a list (typically of tuples containing channel and message IDs).
+    Only unique list elements are added.
+    """
+    guild_name, _ = unique_guild_data(guild)
+    if not get_guild_data(data, guild, "hashes"):
+        set_guild_data(data, guild, "hashes", {})
+    for h,posts in hashes.items():
+        if h in get_guild_data(data, guild, "hashes", []):
+            for p in posts:
+                matching_posts = get_guild_data(data, guild, "hashes")[h]
+                if p in matching_posts:
+                    matching_posts.append(p)
         else:
-            for hash2 in data[guild_name]["hashes"]:
-                diff = hash_diff(h, hash2)
-                if diff < max_diff:
-                    print("Found matching hash", h, hash2, diff)
-                    return True
-    return False
+            get_guild_data(data, guild, "hashes", [])[h] = posts
+    save_guild_data(data, guild)
 
+    
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+async def load_data(client) -> dict:
+    data={}
     async for guild in client.fetch_guilds():
         unique_guild, server_file = unique_guild_data(guild)
         if not data.get(unique_guild):
@@ -124,62 +135,170 @@ async def on_ready():
         with open(server_file, "a+") as f:
             f.seek(0)
             try:
-                data[unique_guild] = json.load(f)
+                if path.getsize(server_file) == 0:
+                    print("Created new file for", unique_guild)
+                    json.dump({}, f)
+                    data[unique_guild] = {}
+                else:
+                    data[unique_guild] = json.load(f)
             except json.JSONDecodeError as e:
-                print(e, server_file, "contents:")
                 f.seek(0)
-                print(f.read())
+                contents = f.read()
+                print(e, server_file, "contents:", contents)
                 data[unique_guild] = {}
+                with open(server_file + ".bak", "w") as backup:
+                    backup.write(contents)
+                f.seek(0)
+                json.dump({}, f)
+    return data
+
+
+
+def unique_guild_data(guild):
+    unique_name = "".join([x for x in guild.name if x.isalnum()]) + "_" + str(guild.id)
+    file_name = path.join("data", unique_name + ".json")
+    return (unique_name, file_name)
+
+def check_message(data, message, max_diff=0):
+    embeds = image_hash_from_message(message)
+    guild_name, _ = unique_guild_data(message.guild)
+    for h in embeds["hashes"]:
+        if max_diff == 0:
+            if h in get_guild_data(data, message.guild, "hashes", default=[]):
+                print("Found matching hash", h)
+                return get_guild_data(data, message.guild, "hashes")[h]
+        else:
+            for hash2 in get_guild_data(data, message.guild, "hashes", default=[]):
+                diff = hash_diff(h, hash2)
+                if diff < max_diff:
+                    print("Found matching hash", h, hash2, diff)
+                    return get_guild_data(data, message.guild, "hashes")[h]
+    return None
+
+class Client(discord.Client):
+
+    def check_command(self, message, command):
+        return message.content.startswith(self.command_strings[command])
             
+    def get_args(self, message, command):
+        return message.content[len(self.command_strings[command]):].split()
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    async def on_ready(self):
+        print('We have logged in as {0.user}'.format(client))
+        self.command_strings = default_strings.copy()
+        self.data = await load_data(client)
+                
 
-    if message.content.startswith(command_strings["hello"]):
-        print("Hello there")
-        await message.channel.send('Hello!')
-    elif message.content.startswith(command_strings["scan"]):
-        history_args = {
-            "limit": None
-        }
-        channels = [message.channel]
-        for w in message.content[len(command_strings["scan"]):].split():
-            if w.isdecimal():
-                history_args["limit"] = int(w)
-            elif w == "here":
-                history_args["before"] = message
-            elif w == "all":
-                channels = message.guild.channels
-        for channel in channels:
-            await scan_channel(channel, history_args)
+    async def on_message(self, message):
+        if message.author == client.user:
+            return
 
-    elif message.content.startswith(command_strings["hash"]):
-        if message.reference:
-            m = message.reference.resolved
-            if isinstance(m, discord.DeletedReferencedMessage):
-                await message.channel.send("The message was deleted.")
-                print("The message was deleted.")
-            if m is None:
-                await message.channel.send("Discord refused to find the message this one references.")
-                print("Discord refused to find the message this one references.")
-            elif isinstance(m, discord.Message):
-                await message.channel.send(str(image_hash_from_message(m)))
-        else:
-            await message.channel.send("Reply to a message to trigger this command.")
+        # if message.author == message.guild.owner: #intents or some other junk broke this
+        if message.author.id == message.guild.owner_id:
+            if self.check_command(message, "hello"):
+                print("Hello there")
+                await message.reply('Hello there')
+            elif self.check_command(message, "scan"):
+                history_args = {
+                    "limit": None
+                }
+                channels = [message.channel]
+                for w in self.get_args(message, "scan"):
+                    if w.isdecimal():
+                        history_args["limit"] = int(w)
+                    elif w == "here":
+                        history_args["before"] = message
+                    elif w == "all":
+                        channels = message.guild.channels
+                for channel in channels:
+                    await scan_channel(channel, self.data, history_args)
 
-    elif message.content.startswith(command_strings["hashdiff"]):
-        words = message.content[len(command_strings["hashdiff"]):].split()
-        if len(words) != 2:
-            await message.channel.send("This command needs 2 hashes.")
-        else:
-            await message.channel.send(str(hash_diff(*words)))
+            elif self.check_command(message, "hash"):
+                if message.reference:
+                    m = message.reference.resolved
+                    if isinstance(m, discord.DeletedReferencedMessage):
+                        await message.reply("The message was deleted.")
+                        print("The message was deleted.")
+                    if m is None:
+                        await message.reply("Discord refused to find the message this one references.")
+                        print("Discord refused to find the message this one references.")
+                    elif isinstance(m, discord.Message):
+                        await message.reply(str(image_hash_from_message(m)))
+                else:
+                    await message.reply("Reply to a message to trigger this command.")
 
-        
-    if check_message(message, SAME_DIFF):
-        await message.reply(command_strings["repost_found"])
+            elif self.check_command(message, "hashdiff"):
+                words = get_args(message, "hashdiff")
+                if len(words) != 2:
+                    await message.reply("This command needs 2 hashes.")
+                else:
+                    await message.reply(str(hash_diff(*words)))
+
+            elif self.check_command(message, "diff"):
+                words = get_args(message, "diff")
+                if len(words) != 2:
+                    await message.reply("This command needs 2 message IDs.")
+                else:
+                    try:
+                        m1 = await message.channel.fetch_message(words[0])
+                        m2 = await message.channel.fetch_message(words[1])
+                        h1 = image_hash_from_message(m1)
+                        h2 = image_hash_from_message(m2)
+                        if not h1["hashes"]:
+                            await message.reply("1st message had no hashable images.")
+                        elif not h2["hashes"]:
+                            await message.reply("2nd message had no hashable images.")
+                        else:
+                            await message.reply(str(hash_diff(h1["hashes"][0], h2["hashes"][0])))
+
+                    except:
+                        await message.reply("An error occurred. Are the message IDs valid?")
+            elif self.check_command(message, "enable"):
+                set_guild_data(self.data, message.guild, "enabled", True)
+                await message.reply(f"Enabled repost checking on this server. Note that some channels may still be excluded. Run `{self.command_strings['include']} all` to include all channels.")
+            elif self.check_command(message, "disable"):
+                set_guild_data(self.data, message.guild, "enabled", False)
+                await message.reply("Disabled repost checking on this server.")
+
+            elif self.check_command(message, "include"):
+                args = self.get_args(message, "include")
+                channels = get_guild_data(self.data, message.guild, "included_channels")
+                if args and args[0] == "all":
+                    channels = [c.id for c in message.guild.channels]
+                elif args and args[0] == "none":
+                    channels = []
+                else:
+                    channels.append([c.id for c in message.channel_mentions])
+                set_guild_data(self.data, message.guild, "included_channels", channels)
+                await message.reply("Checking the following channels for reposts:" + ", ".join([str(message.guild.get_channel(c)) for c in channels]))
+                
+            elif self.check_command(message, "exclude"):
+                args = self.get_args(message, "exclude")
+                channels = get_guild_data(self.data, message.guild, "included_channels")
+                if args and args[0] == "all":
+                    channels = []
+                elif args and args[0] == "none":
+                    channels = [c.id for c in message.guild.channels]
+                else:
+                    try:
+                        channels.remove([c.id for c in message.channel_mentions])
+                    except ValueError:
+                        await message.reply("A mentioned channel was not in the list. List not updated.")
+
+                set_guild_data(self.data, message.guild, "included_channels", channels)
+                await message.reply("Checking the following channels for reposts:" + ", ".join([str(message.guild.get_channel(c)) for c in channels]))
+
+            
+        if message.channel.id in get_guild_data(self.data, message.guild, "included_channels") and (match := check_message(self.data, message, SAME_DIFF)):
+            jump = None
+            try:
+                channel = message.guild.get_channel(match[0][0])
+                jump = await channel.fetch_message(match[0][1])
+                jump =  " " + jump.jump_url
+            finally:
+                await message.reply(self.command_strings["repost_found"] + (jump if jump else ""))
     
+client = Client()
 
 token = environ.get("REPOSTI_DISCORD_TOKEN")
 if token:
